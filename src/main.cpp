4379caa4b43c1b26a09afae553246131bd0d5910
4ohblack-dev <4oh.black@gmail.com>
2026-06-04 18:00:43 +0900

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,50 @@
 #include<Adafruit_AS5600.h>
 
 Adafruit_AS5600 as5600;
+int16_t loopCount = 0;
+uint16_t lastRawAngle = 0;
+bool isfirstRead = true;
 
 void setup(){
   Wire.begin(21,22);
   Serial.begin(115200);
+
   if (as5600.begin()==false){
-    Serial.print("AS5600 is not detected");
+    Serial.println("AS5600 is not detected");
   }
-  Serial.print("AS5600 is detected");
+
+  Serial.println("AS5600 is detected");
+  if(as5600.isMagnetDetected()){
+    Serial.println("good magnet_Position");
+  }
+  if(as5600.isAGCminGainOverflow()){
+    Serial.println("magnet is too strong");
+  }
+  if(as5600.isAGCmaxGainOverflow()){
+    Serial.println("magnet is too weak");
+  }
+
+  Serial.println("as5600 PERFECT");
   delay(100);
+}
+
+
+void loop(){
+  uint16_t currentRawAngle = as5600.getRawAngle();//連続して回るようなところ
+  if(isfirstRead){
+    lastRawAngle=currentRawAngle;
+    isfirstRead=false;
+  }
+  int16_t diff = currentRawAngle - lastRawAngle;
+  if(diff<-2048)loopCount++;
+  else if (diff>2048)loopCount--;
+
+  lastRawAngle=currentRawAngle;
+
+  int32_t totalsteps=((int32_t)loopCount*4096)+currentRawAngle;
+  float totalDegree = totalsteps*360/4096;
+
+
+  float degrees = currentRawAngle*360/4095;
+  delay(5);
 }
